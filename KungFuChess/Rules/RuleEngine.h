e38a142a6380feb11198d2196e77ad6d77f8eaa7
Sari <S0533185873@gmail.com>
2026-07-13 23:34:57 +0300

    itaration 6 - final fix rule_engine

--- KungFuChess/Rules/RuleEngine.h
+++ KungFuChess/Rules/RuleEngine.h
@@ -1,18 +1,13 @@
 #pragma once
-#include "Board.h"
+#include "../Model/Board.h"
 #include "MoveValidation.h"
 #include "PieceRules.h"
-#include "Position.h"
+#include "../Model/Position.h"
 
-// RuleEngine answers one question: given a source and destination cell, is
-// this move legal right now? It is read-only with respect to Board - it
-// never mutates it, starts motions, or knows about game-over. Piece-specific
-// movement geometry is fully delegated to the matching PieceRules subclass.
 class RuleEngine {
 public:
     static MoveValidation validateMove(const Board& board, const Position& src, const Position& dst);
 
 private:
-    // Returns the stateless rule-object strategy for a given piece kind.
     static const PieceRules& rulesFor(PieceKind kind);
 };
